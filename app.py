f6edd6b9a1a5b45a8d687dd7e706cce6f8d6d25d
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-04-27 00:48:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ df1=pd.read_csv('B.csv')
 df2=pd.read_csv('C.csv')
 #st.sidebar.title("Menu")
 state_select = st.selectbox('Select a state',df['State'].unique())
-energy_select=st.selectbox('Select a Energy Type',df1['EnergySourceType'].unique())
+
 selected_state = df[df['State']==state_select]
-energy=df1[df1['EnergySourceType']==energy_select]
+
 st.markdown('''
 <div class="jumbotron text-center" style='background-color: #fff'>
   <h1 style="margin: auto; width: 100%;">Renewable Energy Dashboard</h1>
@@ -22,6 +22,8 @@ st.markdown('''
 ''', unsafe_allow_html=True);
 a=px.bar(selected_state,x='YearValue',y='Generation_GWh',color='EnergySource',barmode='group')
 st.plotly_chart(a)
+energy_select=st.selectbox('Select a Energy Type',df1['EnergySourceType'].unique())
+energy=df1[df1['EnergySourceType']==energy_select]
 st.markdown("## **Energy Generation By Source**")
 b=px.bar(energy,x='YearValue',y='Generation_GWh',color='EnergySource',barmode='group')
 st.plotly_chart(b)
