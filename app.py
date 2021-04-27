1687ec8cede9c41ef6bb6926f5dbd93e988266a1
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-04-26 23:59:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,8 @@ import plotly.graph_objects as go
 df=pd.read_csv('A.csv')
 df1=pd.read_csv('B.csv')
 df2=pd.read_csv('C.csv')
+st.sidebar.title("Menu")
 state_select = st.sidebar.selectbox('Select a state',df['State'].unique())
 selected_state = df[df['State']==state_select]
+a=px.bar(selected_state,x='YearValue',y='Generation_GWh',color='EnergySource',barmode='group')
+st.plotly_chart(a)
