d2ea14f01d0e5f736ce4d6c1351f5adacc657782
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-04-27 02:13:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ if visualization=='Generation':
   st.plotly_chart(c)
 elif visualization=='Capacity':
   st.markdown("## **Energy Potential By State And Source**")
-  state_select = st.selectbox('Select a state',df3['State'].unique().sort_values().tolist())
+  state_select = st.selectbox('Select a state',df3['State'].unique())
   selected_state = df3[df3['State']==state_select]
   a=px.bar(selected_state,x='YearValue',y='Capacity_MW',color='EnergySource',barmode='group')
   st.plotly_chart(a)
