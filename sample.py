1298ddcf3c08930949ffea6a0bab4c63e3e649ff
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2022-01-04 17:57:15 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,6 +2,5 @@ import streamlit as st
 import pandas as pd
 your_name = st.text_input("Number")
 df=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vR0UpG1xhHKc8BVAFyGr-e8PPlwnwY0_7I7KXx0AxL1s3V3StO4YVA8mRnACBE7FyFXD9HPMqI4yh7r/pub?gid=0&single=true&output=csv')
-for df['phone'] in your_name:
-  your_name==df['phone']
-  print('ff')
+al=your_name in df['phone']
+print('al')
