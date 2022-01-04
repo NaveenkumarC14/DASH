63259663cc37ac4624ab13f9fe9cc70336cddf27
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2022-01-04 18:22:10 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-df=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vR0UpG1xhHKc8BVAFyGr-e8PPlwnwY0_7I7KXx0AxL1s3V3StO4YVA8mRnACBE7FyFXD9HPMqI4yh7r/pub?gid=0&single=true&output=csv')
+df=pd.read_csv('Untitled spreadsheet - Sheet1.csv')
 df
 your_name = st.text_input("Number")
 
