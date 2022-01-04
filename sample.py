c1018356b060c6f7d63b130b9cd2dd69a63d9006
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2022-01-04 18:01:09 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 your_name = st.text_input("Number")
 df=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vR0UpG1xhHKc8BVAFyGr-e8PPlwnwY0_7I7KXx0AxL1s3V3StO4YVA8mRnACBE7FyFXD9HPMqI4yh7r/pub?gid=0&single=true&output=csv')
-if your_name in df:
-  print('al')
+if 'your_name' in df.columns:
+   print("Courses column is present : Yes")
+else:
+   print("Courses column is present : No")
 
