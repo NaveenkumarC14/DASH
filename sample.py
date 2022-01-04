5dc3a824fa9824896603c202891f063e3896ce1b
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2022-01-04 18:33:17 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,7 +4,7 @@ import numpy as np
 df=pd.read_csv('Untitled spreadsheet - Sheet1.csv')
 df
 your_name = st.text_input("Number")
-if 'your_name' in df['phone']:
+if 'your_name' in df.columns:
     print("Yes")
 else:
     print("Nope")
