79d2ce55dff80a6b63768d5bc00e80638fad8d46
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2022-01-05 16:12:26 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,13 +3,6 @@ import pandas as pd
 import numpy as np
 df=pd.read_csv('Untitled spreadsheet - Sheet1.csv')
 df
-your_name = st.text_input("Number")
-def search(your_name, df['phone']):
-    for i in range(len(list)):
-        if list[i] == your_name:
-            return True
-    return False
-if search(df['phone'],your_name):
-    print("Platform is found")
-else:
-    print("Platform does not found")
+#your_name = st.text_input("Number")
+a=df[df['A'] == st.text_input("Number")]
+a
