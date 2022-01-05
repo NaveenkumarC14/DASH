675bda15700b425dabac259ec6d47119b54df018
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2022-01-05 17:04:22 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,12 +4,12 @@ import numpy as np
 df=pd.read_csv('Untitled spreadsheet - Sheet1.csv')
 df
 
-your_name = st.text_input("Number")
+#your_name = st.text_input("Number")
 #if your_name:
 #  df[df['phone'] == your_name]
     #st.write("There is some value. Processing...")
 
 mytext = st.text_input('enter text')
-st.write(pd.DataFrame({'df' : [mytext,mytext,mytext]}))
+st.write(pd.DataFrame({'df' : [' ']}))
 
 
