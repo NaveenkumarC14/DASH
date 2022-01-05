5798813ceb5e2aac6ec8ccb2b9d35e1da32c563f
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2022-01-05 19:41:08 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -10,15 +10,7 @@ import numpy as np
     #st.write("There is some value. Processing...")
 
 df=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSjdA5Ku8OzMsGEStlOKciPERkgSLoCYxPObTbFbblm85P0xDtZM5C7fHsrgpEL0pdQCsrz-F_G-Rz3/pub?gid=1840897213&single=true&output=csv")
-#print(df['Contact Number'])
-#your_name=st.text_input("Number",value=8075311214)
-#res = df.isin([your_name]).any().any()
 
-#if res :
- #  st.write("This value exists in Dataframe")
-
-#else :
-#   st.write("This value does not exists in Dataframe")
 if __name__ == '__main__':
     input = st.empty()
     txt = input.text_input("Insert number:")
