0dfd9d106a521f4e0975b6c7fd5bbb3a4a7feb09
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2022-01-05 16:13:22 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,6 +3,6 @@ import pandas as pd
 import numpy as np
 df=pd.read_csv('Untitled spreadsheet - Sheet1.csv')
 df
-#your_name = st.text_input("Number")
-a=df[df['A'] == st.text_input("Number")]
+your_name = st.text_input("Number")
+a=df[df['A'] == your_name]
 a
