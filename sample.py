18431a5be72e5960a7edf8bf68cede3b9af9eeab
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2022-01-06 12:11:30 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -30,9 +30,10 @@ if __name__ == '__main__':
         else :
             st.markdown('''
 <div class="jumbotron text-center" style='background-color: #fff'>
-  <h1 style="margin: auto; width: 100%;">This value does not exists in Dataframe</h1>
+  <h1></h1><p style="margin: auto; font-weight: 400; text-align: center; width: 100%;">This value does not exists in Dataframe</p>
 </div>
  ''', unsafe_allow_html=True);
+
           #st.write("This value does not exists in Dataframe")
     
     
