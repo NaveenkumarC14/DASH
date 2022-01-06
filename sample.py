f13ffa3fbd7c61558025ade5695221ec1c48accf
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2022-01-06 12:06:49 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -8,7 +8,7 @@ import numpy as np
 #if your_name:
 #  df[df['phone'] == your_name]
     #st.write("There is some value. Processing...")
-@st.cache
+
 df=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSjdA5Ku8OzMsGEStlOKciPERkgSLoCYxPObTbFbblm85P0xDtZM5C7fHsrgpEL0pdQCsrz-F_G-Rz3/pub?gid=1840897213&single=true&output=csv")
 
 if __name__ == '__main__':
